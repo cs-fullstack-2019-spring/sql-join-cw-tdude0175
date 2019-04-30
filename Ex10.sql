1a8c55b8b83562d76a244ed0202916e200081060
tdude0175 <tdude0175@gmail.com>
2019-04-30 12:02:46 -0500

    completed problem 10

--- Ex10.sql
+++ Ex10.sql
@@ -1,8 +1,10 @@
 -- Show the average each person pays for rent each year.-- SQL
 
+select * from people join address on people.id = address.id;
 
-select name, updatedate,avg(rent)
+
+select name,avg(rent*12) as "yearly rent"
 from people join address on people.id = address.id
-group by name, updatedate
-order by name,updatedate
+group by name
+order by name
 ;
